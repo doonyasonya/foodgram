8df580b0a466e1da3a900702d122845caab797a2
doonyasonya <doonyanikitin@yandex.ru>
2024-08-29 07:50:54 +0300

    Сериализаторы 2

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -33,3 +33,48 @@ class FoodgramUserSerializer(serializers.ModelSerializer):
         user = self.context['request'].user
         if user.is_authenticated:
             return user.followers.filter(author=author).exists()
+        return False
+
+
+class FollowRecipesSerializer(serializers.ModelSerializer):
+    """Сериализатор рецепта подписки."""
+
+    class Meta:
+        model = Recipe
+        fields = (
+            'id',
+            'name',
+            'image',
+            'cooking_time',
+        )
+
+
+class FoodgramFollowSerializer(serializers.ModelSerializer):
+    """Сериализатор подписки пользователя."""
+
+    is_subscribed = serializers.SerializerMethodField()
+    recipes = serializers.SerializerMethodField()
+    recipes_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = FoodgramUser
+        fields = (
+            'id',
+            'email',
+            'username',
+            'password',
+            'first_name',
+            'last_name',
+            'avatar',
+            'is_subscribed',
+            'recipes',
+            'recipes_count'
+        )
+
+    def get_is_subscribed(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            return FoodgramFollow.objects.filter(user=user, author=obj).exists()
+        return False
+
+    def get_recipes(self, obj):
