87a4db2f87b2d945d74e3d1c4b031d3f6cb2a590
doonyasonya <doonyanikitin@yandex.ru>
2024-10-13 23:33:41 +0300

    Воркфлоу 12

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
 
 INSTALLED_APPS = [
     "django.contrib.admin",
