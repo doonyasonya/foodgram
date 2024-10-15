9d20a97c37810fa482de55aab82290429bf7d95b
doonyasonya <doonyanikitin@yandex.ru>
2024-10-15 20:27:15 +0300

    Delete all

--- backend/users/pagination.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.pagination import PageNumberPagination
-
-from .constants import PAGE_SIZE
-
-
-class UserPagination(PageNumberPagination):
-    page_size = PAGE_SIZE
-    page_size_query_param = "limit"
