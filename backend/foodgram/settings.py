da44d5a90ff092fde2eff8bb491d40ee7d4ce8c1
doonyasonya <doonyanikitin@yandex.ru>
2024-11-30 23:22:38 +0300

    site-4

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -105,10 +105,10 @@ USE_TZ = True
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = 'media'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = 'static'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
